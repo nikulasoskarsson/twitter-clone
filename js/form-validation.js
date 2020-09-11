9d77dcb74198dcd9607b8359735e3c72dc0dc0fd
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-09-11 17:39:24 +0200

    refactoring and adding to signup frontend validation

--- js/form-validation.js
+++ js/form-validation.js
@@ -29,52 +29,75 @@ function getAllFormFields() {
 
 function addFormListeners(formFields) {
   formFields.forEach((formfield) => {
-    formfield.addEventListener('keypress', (e) => checkForErrors(e));
+    if (
+      formfield.name == 'firstname' ||
+      formfield.name == 'lastname' ||
+      formfield.name == 'username'
+    ) {
+      formfield.addEventListener('keyup', (e) =>
+        updateCharCount(e.target.name, e.target.value.length)
+      );
+    }
+    if (formfield.name == 'email') {
+      formfield.addEventListener('blur', (e) => checkEmail(e));
+    }
   });
 }
 
-function checkForErrors(event) {
-  const fieldName = event.target.name;
-
-  if (
-    fieldName == 'firstname' ||
-    fieldName == 'lastname' ||
-    fieldName == 'username'
-  ) {
-    updateCharCount(fieldName, event.target.value.length);
-  }
-}
 function updateCharCount(fieldName, count) {
   const fieldFromDom = signupForm[`${fieldName}`];
   const containerDiv = fieldFromDom.parentNode;
-  console.log(containerDiv);
   const charCountDiv = fieldFromDom.parentNode.lastChild.previousElementSibling; // Div containting the error msg
-
   const charCountSpan = charCountDiv.lastChild.previousElementSibling; // Span containting the dynamic counter
+
   charCountSpan.innerText = count;
   charCountDiv.classList.add(count < 50 ? 'success' : 'fail');
+
   if (count < 5) {
-    charCountDiv.classList.add('fail');
-    charCountDiv.classList.remove('success');
-    containerDiv.classList.add('border-fail');
-
-    containerDiv.classList.remove('border-nutural');
-    containerDiv.classList.remove('border-success');
+    showErrorCharCount(charCountDiv, containerDiv);
   } else if (count > 5 && count < 50) {
-    charCountDiv.classList.add('success');
-    charCountDiv.classList.remove('fail');
-    containerDiv.classList.add('border-success');
+    showSuccessCharCount(charCountDiv, containerDiv);
+  } else {
+    showErrorCharCount(charCountDiv, containerDiv);
+  }
+}
 
+function showErrorCharCount(charCountDiv, containerDiv) {
+  charCountDiv.classList.add('fail');
+  charCountDiv.classList.remove('success');
+  containerDiv.classList.add('border-fail');
+
+  containerDiv.classList.remove('border-nutural');
+  containerDiv.classList.remove('border-success');
+}
+function showSuccessCharCount(charCountDiv, containerDiv) {
+  charCountDiv.classList.add('success');
+  charCountDiv.classList.remove('fail');
+  containerDiv.classList.add('border-success');
+
+  containerDiv.classList.remove('border-nutural');
+  containerDiv.classList.remove('border-fail');
+}
+
+function checkEmail(e) {
+  const email = e.target.value;
+
+  const containerDiv = e.target.parentNode;
+
+  if (!validateEmailRegex(email)) {
+    containerDiv.classList.add('border-fail');
+    containerDiv.classList.remove('border-success');
+    containerDiv.classList.remove('border-nutural');
+  } else {
+    containerDiv.classList.add('border-success');
     containerDiv.classList.remove('border-nutural');
     containerDiv.classList.remove('border-fail');
-  } else {
-    charCountDiv.classList.add('fail');
-    charCountDiv.classList.remove('success');
-    containerDiv.classList.add('border-fail');
-
-    containerDiv.classList.remove('border-nutural');
-    containerDiv.classList.remove('border-success');
   }
+}
+
+function validateEmailRegex(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 function checkLength(input, min, max) {}
--- php/components/signup-modal.php
+++ php/components/signup-modal.php
@@ -1,5 +1,5 @@
-<div id="modal-container" class="modal-container display-hidden">
-    <div id="signup-modal" class="modal signup-modal display-hidden">
+<div id="modal-container" class="modal-container ">
+    <div id="signup-modal" class="modal signup-modal ">
 
 
         <form action="php/actions/signup.php" id="signup-form" class="signup-form" method="POST">
@@ -22,35 +22,35 @@
             </div>
 
 
-            <div class="signup-form__input-field form-input-field">
+            <div class="signup-form__input-field form-input-field border-nutural">
                 <label for="" class="signup-form__label form-input-field__label">Lastname</label>
                 <input type="text" name="lastname" id="lastname" class="signup-form__input form-input-field__input" />
                 <div id="lastname-character-display"><span id="lastname-character-count">0</span> / 50</div>
             </div>
 
 
-            <div class="signup-form__input-field form-input-field">
+            <div class="signup-form__input-field form-input-field border-nutural">
                 <label for="" class="signup-form__label form-input-field__label">Choose a username</label>
                 <input type="text" name="username" id="username" class="signup-form__input form-input-field__input" />
                 <div id="lastname-character-display"><span id="lastname-character-count">0</span> / 50</div>
             </div>
-            <div id="username-character-display"><span id="username-character-count">0</span> / 50</div>
 
-            <div class="signup-form__input-field form-input-field">
+
+            <div class="signup-form__input-field form-input-field border-nutural">
                 <label for="" class="signup-form__label form-input-field__label">Email</label>
                 <input type="email" name="email" id="email" class="signup-form__input form-input-field__input" />
             </div>
 
-            <div class="signup-form__input-field form-input-field">
+            <div class="signup-form__input-field form-input-field border-nutural">
                 <label for="" class="signup-form__label form-input-field__label">Password</label>
-                <input type="text" name="password" id="password" class="signup-form__input form-input-field__input" />
+                <input type="password" name="password" id="signup-modal-password" class="signup-form__input form-input-field__input" />
             </div>
 
             <h3 class="signup-form__input-dob-heading heading-secondary">Date of birth</h3>
             <p class="signup-form__input-dob-text text-xs-light">This will not be shown publicly. Confirm your own age, even if this account is for a bussiness, pet or something else.</p>
 
             <div class="signup-form__dob-container">
-                <div class="signup-form__dob-input-field form-select-input-field">
+                <div class="signup-form__dob-input-field form-select-input-field border-nutural">
                     <label for="" class="signup-form__dob-label form-select-input-field__label">Month</label>
                     <select id="select-month" type="text" name="month" class="signup-form__dob-input form-select-input-field__input">
                         <option value="" selected hidden></option>
@@ -58,14 +58,14 @@
                 </div>
 
 
-                <div class="signup-form__dob-input-field form-select-input-field">
+                <div class="signup-form__dob-input-field form-select-input-field border-nutural">
                     <label for="" class="signup-form__dob-label form-select-input-field__label">Year</label>
                     <select id="select-year" type="text" name="year" class="signup-form__dob-input form-select-input-field__input">
                         <option value="" selected hidden></option>
                     </select>
                 </div>
 
-                <div class="signup-form__dob-input-field form-select-input-field">
+                <div class="signup-form__dob-input-field form-select-input-field border-nutural">
                     <label for="" class="signup-form__dob-label form-select-input-field__label">Day</label>
                     <select id="select-day" type="text" name="day" class="signup-form__dob-input form-select-input-field__input">
                         <option value="" selected hidden></option>
