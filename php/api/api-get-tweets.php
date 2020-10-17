27d68c10812f3382f8850f5c937cc5a2c36cf54a
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-10-17 11:36:58 +0200

    get tweets api working in postman with multiple images

--- php/api/api-get-tweets.php
+++ php/api/api-get-tweets.php
@@ -6,15 +6,34 @@ $apiHelper->validateUserId($_GET); // Exit if not provided with existing id of a
 
 require(__DIR__ . '/../private/db.php');
 
-$userId = $_GET['userId'];
-$query = $db->prepare("SELECT * FROM tweets WHERE user_id =$userId");
+
+$query = $db->prepare("SELECT tweets.id, tweets.timestamp, tweet_body.body FROM tweets 
+LEFT OUTER JOIN tweet_body ON tweet_body.tweet_id = tweets.id 
+WHERE user_id = :id");
+$query->bindValue(':id',$_GET['userId']);
 $query->execute();
+
 $rows = $query->fetchAll();
 
-$apiHelper->sendResponse(200, json_encode($rows));
+foreach($rows as &$row){
+     if(isset($row[0])){
+        $row['images'] = array();
+        $query = $db->prepare("SELECT tweet_id, url FROM tweet_images WHERE tweet_id = :id");
+        $query->bindValue(':id',$row[0]);
+      
+        $query->execute();
+        $imgRows = $query->fetchAll();
+        foreach($imgRows as $imgRow){
+            array_push($row['images'], $imgRow[1]);
+        }
+     }
+   
+}
+
+ $apiHelper->sendResponse(200, json_encode($rows));
 
 
 // SELECT * FROM tweets 
 // LEFT OUTER JOIN tweet_body ON tweet_body.tweet_id = tweets.id 
 // LEFT OUTER JOIN tweet_images ON tweet_images.tweet_id = tweets.id
-// WHERE user_id = 4
+// WHERE user_id = :id
