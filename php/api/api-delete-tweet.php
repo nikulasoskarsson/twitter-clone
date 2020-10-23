0311c1acbedc18d9ae94cb0118795c93eead4270
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-10-23 18:04:53 +0200

    del endpoint working

--- php/api/api-delete-tweet.php
+++ php/api/api-delete-tweet.php
@@ -6,8 +6,8 @@ $apiHelper = new ApiHelper();
 $apiHelper->validateUserId($_POST); // Exit if not provided with existing id of a user
 
 
-require_once(__DIR__. '/../private/db.php');
-$query = $db->prepare('"DELETE FROM tweets WHERE id = :id');
+require(__DIR__. '/../private/db.php');
+$query = $db->prepare('DELETE FROM tweets WHERE id = :id');
 $query->bindValue(':id', $_POST['tweetId']);
 $query->execute();
 
